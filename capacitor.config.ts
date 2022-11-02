76efe6ed7174bb94f3802c64dfefcfee0aef7d6a
codscript <mussa.monteiro@gmail.com>
2022-11-02 11:51:13 -0300

    MECAPECAS: Primeiro commit criação do app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UNICEU-Projeto-Integrador-III',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
